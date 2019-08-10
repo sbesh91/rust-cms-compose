93d24677be55136ffbab1cf5628d95693dfdd150
Steven Beshensky <s.beshensky@gmail.com>
2019-08-09 19:23:50 -0500

    fix: update init script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sbesh.com www.sbesh.com)
+domains=(sbe.sh www.sbe.sh)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="s.beshensky@gmail.com" # Adding a valid address is strongly recommended
